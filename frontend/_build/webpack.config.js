4b463e00482ec4e1a5742e357913a1ff893cc3f6
Ingo Fahrentholz <ifahrentholz@pro-vision.de>
2018-04-24 19:06:46 +0200

    Included ES6 transpiling

--- frontend/_build/webpack.config.js
+++ frontend/_build/webpack.config.js
@@ -3,25 +3,39 @@ const path = require("path");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = function(env, argv) {
 
   this.isProduction = argv.mode === "production";
+  this.productionPlugins = [];
+
+  if (this.isProduction) {
+    this.productionPlugins.push(
+      new UglifyJsPlugin({
+        sourceMap: true
+      })
+    );
+  }
 
   return {
     context: path.resolve(__dirname),
-    devtool: "source-map",
+    devtool: this.isProduction ? "source-map" : "",
 
-    entry: ["../src/js/index.js", "../src/styles/styles.scss"],
+    entry: [
+      "../src/js/index.js",
+      "../src/styles/styles.scss"
+    ],
 
     output: {
       path: path.resolve(__dirname, "../dist/js"),
-      filename: "[name].js",
-      libraryTarget: "umd"
+      filename: "index.js",
+      libraryTarget: "umd",
+      chunkFilename: "[name].js"
     },
 
     plugins: [
+      // Delete dist folder
       new CleanWebpackPlugin([
         "../dist"
       ], {
@@ -30,38 +44,56 @@ module.exports = function(env, argv) {
         dry: false
       }),
 
-      new UglifyJsPlugin({
-        sourceMap: true
-      }),
-
+      // Create a [name].css file in filesystem
       new MiniCssExtractPlugin({
         filename: "../styles/[name].css",
         chunkFilename: "../styles/[id].css",
       }),
 
-      new OptimizeCssAssetsPlugin({
-        assetNameRegExp: /\.css$/g,
-        cssProcessor: require("cssnano"),
-        cssProcessorOptions: {
-          map: {
-            inline: false
-          },
-          discardComments: {
-            removeAll: true
-          }
-        },
-        canPrint: true,
-      })
+      new CopyWebpackPlugin([
+        { from: '../src/assets/**/*', to: '../', context: "../src" },
+      ]),
+
+      ...this.productionPlugins
     ],
 
     module: {
       rules: [
+        // Configure ES6 loader
+        {
+          test: /\.js$/,
+          exclude: "/node_modules/",
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        // Configure scss loader
         {
           test: /\.scss$/,
           use: [
             MiniCssExtractPlugin.loader,
-            "css-loader",
-            "sass-loader",
+            {
+              loader: "css-loader",
+              options: {
+                minimize: false,
+                sourceMap: true
+              }
+            },
+            {
+              loader: "postcss-loader",
+              options: {
+                sourceMap: true,
+                config: {
+                  path: "../_build/postcss.config.js"
+                }
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true
+              }
+            },
           ],
         }
       ]
