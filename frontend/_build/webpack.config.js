0eb9c68eb34a4bc493cc823c3a4670cb31049c49
Ingo Fahrentholz <ifahrentholz@pro-vision.de>
2018-04-24 12:39:45 +0200

    implement scss support + map + minimize

--- frontend/_build/webpack.config.js
+++ frontend/_build/webpack.config.js
@@ -2,44 +2,69 @@ const webpack = require("webpack");
 const path = require("path");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = function(env, argv) {
 
   this.isProduction = argv.mode === "production";
 
-  this.productionPlugins = [
-    new UglifyJsPlugin({
-      sourceMap: true
-    })
-  ];
-
-  this.commonPlugins = [
-    new CleanWebpackPlugin([
-      "../dist"
-    ], {
-      allowExternal: true,
-      verbose: true,
-      dry: false
-    }),
-  ];
-
   return {
     context: path.resolve(__dirname),
-    devtool: this.isProduction ? "source-map" : "",
+    devtool: "source-map",
 
-    entry: {
-      "app": "../src/js/index.js"
-    },
+    entry: ["../src/js/index.js", "../src/styles/styles.scss"],
 
     output: {
       path: path.resolve(__dirname, "../dist/js"),
-      publicPath: "src/",
       filename: "[name].js",
       libraryTarget: "umd"
     },
 
-    plugins: this.isProduction
-      ? [...this.commonPlugins, ...this.productionPlugins]
-      : [...this.commonPlugins]
+    plugins: [
+      new CleanWebpackPlugin([
+        "../dist"
+      ], {
+        allowExternal: true,
+        verbose: true,
+        dry: false
+      }),
+
+      new UglifyJsPlugin({
+        sourceMap: true
+      }),
+
+      new MiniCssExtractPlugin({
+        filename: "../styles/[name].css",
+        chunkFilename: "../styles/[id].css",
+      }),
+
+      new OptimizeCssAssetsPlugin({
+        assetNameRegExp: /\.css$/g,
+        cssProcessor: require("cssnano"),
+        cssProcessorOptions: {
+          map: {
+            inline: false
+          },
+          discardComments: {
+            removeAll: true
+          }
+        },
+        canPrint: true,
+      })
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader",
+          ],
+        }
+      ]
+    },
   }
 }
--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -1,16 +1,10 @@
 
 /*!
-     _ _      _       _
- ___| (_) ___| | __  (_)___
-/ __| | |/ __| |/ /  | / __|
-\__ \ | | (__|   < _ | \__ \
-|___/_|_|\___|_|\_(_)/ |___/
-                   |__/
- Version: 1.9.0
-  Author: Ken Wheeler
- Website: http://kenwheeler.github.io
-    Docs: http://kenwheeler.github.io/slick
-    Repo: http://github.com/kenwheeler/slick
-  Issues: http://github.com/kenwheeler/slick/issues
+  An important License comment here
  */
 console.log(`A silly entry point`);
+
+/**
+ * Another comment here - not important
+ */
+console.log("getting worse");
